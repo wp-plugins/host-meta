0c33e05a68ce29ff6aa9cc86428fb42cb80a5b38
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-27 18:08:11 +0000

    fixed ostatus compatibility issue: http://status.net/open-source/issues/3235
    git-svn-id: https://plugins.svn.wordpress.org/host-meta/trunk@401784 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: host-meta
 Plugin URI: http://notizblog.org/
 Description: Host Metadata for WordPress (IETF-Draft: http://tools.ietf.org/html/draft-hammer-hostmeta)
-Version: 0.4
+Version: 0.4.1
 Author: Matthias Pfefferle
 Author URI: http://notizblog.org/
 */
@@ -23,138 +23,140 @@ class HostMetaPlugin {
    */
   public static function renderHostMeta($query) {
     if (($query["well-known"] == "host-meta") || ($query["well-known"] == "host-meta.json")) {
-	    // get accept header
-	    $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
+      // get accept header
+      $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
 
-      // check format-param and accept header to render the right output format			
-			if (in_array('application/json', $accept) || ($query["well-known"] == "host-meta.json")) {
-				// render json version
-				self::renderJrd();
-			} else {
-				// render xml version
-				self::renderXrd();
-			}
-			
-			// that's it
-			exit;
+      // check format-param and accept header to render the right output format     
+      if (in_array('application/json', $accept) || ($query["well-known"] == "host-meta.json")) {
+        // render json version
+        self::renderJrd();
+      } else {
+        // render xml version
+        self::renderXrd();
+      }
+      
+      // that's it
+      exit;
     }
   }
 
-	/**
-	 * renders the host-meta file in xml
+  /**
+   * renders the host-meta file in xml
    */
   public static function renderXrd() {
-	  header('Content-Type: application/xrd+xml; charset=' . get_option('blog_charset'), true);
-		$host_meta = self::generateContent();
-		
-		echo "<?xml version='1.0' encoding='".get_option('blog_charset')."'?>\n";
+    header('Content-Type: application/xrd+xml; charset=' . get_option('blog_charset'), true);
+    $host_meta = self::generateContent();
+    
+    echo "<?xml version='1.0' encoding='".get_option('blog_charset')."'?>\n";
     echo "<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'\n";
+    echo "     xmlns:hm='http://host-meta.net/xrd/1.0'\n";
       // add xml-only namespaces
       do_action('host_meta_ns');
     echo ">\n";
+    echo "  <hm:Host>".parse_url(get_option('siteurl'), PHP_URL_HOST)."</hm:Host>\n";
 
     echo self::jrdToXrd($host_meta);
-		  // add xml-only content
-	  	do_action('host_meta_xrd');
-		
+      // add xml-only content
+      do_action('host_meta_xrd');
+    
     echo "\n</XRD>";
-	}
-	
-	/**
-	 * renders the host-meta file in json
+  }
+  
+  /**
+   * renders the host-meta file in json
    */
-	public static function renderJrd() {
-	  header('Content-Type: application/json; charset=' . get_option('blog_charset'), true);
-		$host_meta = self::generateContent();
+  public static function renderJrd() {
+    header('Content-Type: application/json; charset=' . get_option('blog_charset'), true);
+    $host_meta = self::generateContent();
 
-		echo json_encode($host_meta);
-	}
-	
-	/**
-	 * generates the host-meta base array (and activate filter)
-	 *
-	 * @return array
-	 */
-	public static function generateContent() {
-		$host_meta = array("subject" => get_option('siteurl'));
-		$host_meta = apply_filters('host_meta', $host_meta);
-		
-		return $host_meta;
-	}
-	
-	/**
-	 * recursive helper to generade the xrd-xml from the jrd array
+    echo json_encode($host_meta);
+  }
+  
+  /**
+   * generates the host-meta base array (and activate filter)
    *
- 	 * @param string $host_meta
- 	 * @return string
-	 */
-	public static function jrdToXrd($host_meta) {
-	  $xrd = null;
+   * @return array
+   */
+  public static function generateContent() {
+    $host_meta = array("subject" => get_option('siteurl'));
+    $host_meta = apply_filters('host_meta', $host_meta);
+    
+    return $host_meta;
+  }
+  
+  /**
+   * recursive helper to generade the xrd-xml from the jrd array
+   *
+   * @param string $host_meta
+   * @return string
+   */
+  public static function jrdToXrd($host_meta) {
+    $xrd = null;
 
-	  foreach ($host_meta as $type => $content) {
-			// print subject
-			if ($type == "subject") {
-				$xrd .= "<Subject>$content</Subject>";
-				continue;
-			}
-			
-			// print aliases
-			if ($type == "aliases") {
-			  foreach ($content as $uri) {
-			    $xrd .= "<Alias>".htmlentities($uri)."</Alias>";
-			  }
-			  continue;
-			}
-			
-			// print properties
-			if ($type == "properties") {
-			  foreach ($content as $type => $uri) {
-			    $xrd .= "<Property type='".htmlentities($type)."'>".htmlentities($uri)."</Property>";
-			  }
-			  continue;
-			}
-			
-			// print titles
-			if ($type == "titles") {
-			  foreach ($content as $key => $value) {
-			    if ($key == "default") {
-			      $xrd .= "<Title>".htmlentities($value)."</Title>";
-			    } else {
-			      $xrd .= "<Title xml:lang='".htmlentities($key)."'>".htmlentities($value)."</Title>";
-			    }
-			  }
-			  continue;
-			}
-			
-			// print links
-			if ($type == "links") {
-			  foreach ($content as $links) {
-			    $temp = array();
-			    $cascaded = false;
-			    $xrd .= "<Link ";
+    foreach ($host_meta as $type => $content) {
+      // print subject
+      if ($type == "subject") {
+        $xrd .= "<Subject>$content</Subject>";
+        continue;
+      }
+      
+      // print aliases
+      if ($type == "aliases") {
+        foreach ($content as $uri) {
+          $xrd .= "<Alias>".htmlentities($uri)."</Alias>";
+        }
+        continue;
+      }
+      
+      // print properties
+      if ($type == "properties") {
+        foreach ($content as $type => $uri) {
+          $xrd .= "<Property type='".htmlentities($type)."'>".htmlentities($uri)."</Property>";
+        }
+        continue;
+      }
+      
+      // print titles
+      if ($type == "titles") {
+        foreach ($content as $key => $value) {
+          if ($key == "default") {
+            $xrd .= "<Title>".htmlentities($value)."</Title>";
+          } else {
+            $xrd .= "<Title xml:lang='".htmlentities($key)."'>".htmlentities($value)."</Title>";
+          }
+        }
+        continue;
+      }
+      
+      // print links
+      if ($type == "links") {
+        foreach ($content as $links) {
+          $temp = array();
+          $cascaded = false;
+          $xrd .= "<Link ";
 
-			    foreach ($links as $key => $value) {
-  			    if (is_array($value)) {
-  			      $temp[$key] = $value;
-  			      $cascaded = true;
-  			    } else {
-  			      $xrd .= htmlentities($key)."='".htmlentities($value)."' ";
-  			    }
-			    }
+          foreach ($links as $key => $value) {
+            if (is_array($value)) {
+              $temp[$key] = $value;
+              $cascaded = true;
+            } else {
+              $xrd .= htmlentities($key)."='".htmlentities($value)."' ";
+            }
+          }
           if ($cascaded) {
-  			    $xrd .= ">";
+            $xrd .= ">";
             $xrd .= self::jrdToXrd($temp);
-  			    $xrd .= "</Link>";
-  			  } else {
-  			    $xrd .= " />";
-  			  }
-			  }
-			  
-			  continue;
-			}
-		}
-		
-		return $xrd;
-	}
+            $xrd .= "</Link>";
+          } else {
+            $xrd .= " />";
+          }
+        }
+        
+        continue;
+      }
+    }
+    
+    return $xrd;
+  }
 }
 ?>
