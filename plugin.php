f7048f2baeb839c21ef844f03450ba71744b626d
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-14 10:38:57 +0000

    some small bugfixes
    git-svn-id: https://plugins.svn.wordpress.org/host-meta/trunk@838303 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: host-meta
 Plugin URI: http://wordpress.org/extend/plugins/host-meta/
 Description: Host Metadata for WordPress (RFC: http://tools.ietf.org/html/rfc6415)
-Version: 1.0.3
+Version: 1.0.4
 Author: Matthias Pfefferle
 Author URI: http://notizblog.org/
 */
@@ -20,38 +20,38 @@ class HostMetaPlugin {
    * @param array $vars
    * @return array
    */
-  function query_vars($vars) {
+  public static function query_vars($vars) {
     $vars[] = 'well-known';
     $vars[] = 'format';
 
     return $vars;
   }
-  
+
   /**
    * Add rewrite rules
    *
-   * @param WP_Rewrite
+   * @param WP_Rewrite $wp_rewrite
    */
-  function rewrite_rules($wp_rewrite) {
+  public static function rewrite_rules($wp_rewrite) {
     $host_meta_rules = array(
-      '(.well-known/host-meta.json)' => 'index.php?well-known=host-meta',
-      '(.well-known/host-meta)' => 'index.php?well-known=host-meta.json',
+      '(.well-known/host-meta.json)' => 'index.php?well-known=host-meta.json',
+      '(.well-known/host-meta)' => 'index.php?well-known=host-meta',
     );
 
     $wp_rewrite->rules = $host_meta_rules + $wp_rewrite->rules;
   }
-  
+
   /**
    * renders the output-file
    *
-   * @param array
+   * @param array $wp
    */
-  function parse_request($wp) {
+  public static function parse_request($wp) {
     // check if "host-meta" param exists
     if (!array_key_exists('well-known', $wp->query_vars)) {
       return;
     }
-    
+
     if ($wp->query_vars["well-known"] == "host-meta") {
       $format = "xrd";
     } elseif ($wp->query_vars["well-known"] == "host-meta.json") {
@@ -59,20 +59,22 @@ class HostMetaPlugin {
     } else {
       return;
     }
-    
+
     $host_meta = apply_filters('host_meta', array(), $wp->query_vars);
-    
+
     do_action("host_meta_render", $format, $host_meta, $wp->query_vars);
     do_action("host_meta_render_{$format}", $host_meta, $wp->query_vars);
   }
-  
+
   /**
    * renders the host-meta file in xml
+   *
+   * @param array $host_meta
    */
-  function render_xrd($host_meta) {
+  public static function render_xrd($host_meta) {
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/xrd+xml; charset=" . get_bloginfo('charset'), true);
-    
+
     echo "<?xml version='1.0' encoding='".get_bloginfo('charset')."'?>\n";
     echo "<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'\n";
     echo "     xmlns:hm='http://host-meta.net/xrd/1.0'\n";
@@ -84,40 +86,43 @@ class HostMetaPlugin {
     echo self::jrd_to_xrd($host_meta);
       // add xml-only content
       do_action('host_meta_xrd');
-    
+
     echo "\n</XRD>";
     exit;
   }
-  
+
   /**
    * renders the host-meta file in json
+   *
+   * @param array $host_meta
    */
-  function render_jrd($host_meta) {
+  public static function render_jrd($host_meta) {
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=" . get_bloginfo('charset'), true);
 
     echo json_encode($host_meta);
     exit;
   }
-  
+
   /**
    * generates the host-meta base array (and activate filter)
    *
+   * @param array $host_meta
    * @return array
    */
-  function generate_default_content($host_meta) {
+  public static function generate_default_content($host_meta) {
     $host_meta = array("subject" => site_url());
-    
+
     return $host_meta;
   }
-  
+
   /**
    * recursive helper to generade the xrd-xml from the jrd array
    *
    * @param string $host_meta
-   * @return string
+   * @return string the genereated XRD file
    */
-  function jrd_to_xrd($host_meta) {
+  public static function jrd_to_xrd($host_meta) {
     $xrd = null;
 
     foreach ($host_meta as $type => $content) {
@@ -126,7 +131,7 @@ class HostMetaPlugin {
         $xrd .= "<Subject>$content</Subject>";
         continue;
       }
-      
+
       // print aliases
       if ($type == "aliases") {
         foreach ($content as $uri) {
@@ -134,7 +139,7 @@ class HostMetaPlugin {
         }
         continue;
       }
-      
+
       // print properties
       if ($type == "properties") {
         foreach ($content as $type => $uri) {
@@ -142,7 +147,7 @@ class HostMetaPlugin {
         }
         continue;
       }
-      
+
       // print titles
       if ($type == "titles") {
         foreach ($content as $key => $value) {
@@ -154,7 +159,7 @@ class HostMetaPlugin {
         }
         continue;
       }
-      
+
       // print links
       if ($type == "links") {
         foreach ($content as $links) {
@@ -172,17 +177,17 @@ class HostMetaPlugin {
           }
           if ($cascaded) {
             $xrd .= ">";
-            $xrd .= self::jrdToXrd($temp);
+            $xrd .= self::jrd_to_xrd($temp);
             $xrd .= "</Link>";
           } else {
             $xrd .= " />";
           }
         }
-        
+
         continue;
       }
     }
-    
+
     return $xrd;
   }
 }
